9210842b3724a4af97e3c92c94dd683ef1758588
César <cesar.gonpar@educa.jcyl.es>
2026-04-15 10:06:24 +0200

    Preparado para deploy en Render con conexión a DB online

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
-// Conexión a MySQL
+// Conexión a MySQL (local + producción)
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',       // tu usuario de MySQL
-  password: '',       // tu contraseña de MySQL
-  database: 'arcade_db'
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'arcade_db',
+  port: process.env.DB_PORT || 3306
 });
 
 // Conectar
