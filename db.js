40ac07f0b1361c4a8a31fc081651a9ab81ac4341
cesargonpar <cesar.gonpar@educa.jcyl.es>
2026-04-28 23:27:22 +0200

    Actualizar db.js

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'arcade_db',
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT || 3306,
 // 🔥 CLAVE para Railway
   ssl: {
     rejectUnauthorized: false
